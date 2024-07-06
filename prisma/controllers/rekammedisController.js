b6f5b78031b190be1f9260ff060e589e4cd9a3ec
MaulanaBhakti <maulana100801@gmail.com>
2024-07-06 18:38:37 +0700

    revisi

--- prisma/controllers/rekammedisController.js
+++ prisma/controllers/rekammedisController.js
@@ -7,36 +7,61 @@ const rekammedisController = {
   adminCRUDRekamMedis: async (req, res) => {
     try {
       // Pastikan user memiliki peran pegawai
-      const { jabatan_admin } = req;
-      if (jabatan_admin !== "admin") {
+      const { user } = req;
+      if (user.role !== "admin") {
         return res
           .status(403)
           .json({ success: false, message: "Unauthorized access" });
       }
 
       const { action, data } = req.body;
+      console.log("Action:", action); // Log data yang diterima
+      console.log("Data diterima:", data);
       let result;
+
       switch (action) {
         case "create":
-          result = await prisma.rekammedis.create({
+          if (
+            !data.id_rekam_medis ||
+            data.id_hewan === 0 ||
+            data.id_pemilik === 0 ||
+            data.id_pegawai === 0 ||
+            data.id_obat === 0 ||
+            !data.keluhan ||
+            !data.diagnosa ||
+            !data.tgl_periksa
+          ) {
+            console.log("Missing or invalid required fields", data); // Log untuk detail lebih lanjut
+            return res
+              .status(400)
+              .json({ success: false, message: "Missing required fields" });
+          }
+          result = await prisma.rekamMedis.create({
             data: {
+              id_hewan: data.id_hewan,
+              id_pemilik: data.id_pemilik,
+              id_pegawai: data.id_pegawai,
+              id_obat: data.id_obat,
               keluhan: data.keluhan,
               diagnosa: data.diagnosa,
               tgl_periksa: data.tgl_periksa,
             },
           });
           break;
+
         case "read":
-          result = await prisma.rekammedis.findMany();
+          result = await prisma.rekamMedis.findMany();
           break;
+
         case "update":
-          result = await prisma.rekammedis.update({
+          result = await prisma.rekamMedis.update({
             where: { id_rekam_medis: data.id_rekam_medis },
             data: { ...data },
           });
           break;
+
         case "delete":
-          result = await prisma.hewan.delete({
+          result = await prisma.rekamMedis.delete({
             where: { id_rekam_medis: data.id_rekam_medis },
           });
           break;
@@ -55,8 +80,8 @@ const rekammedisController = {
   pegawaiCRUDRekamMedis: async (req, res) => {
     try {
       // Pastikan user memiliki peran pegawai
-      const { jabatan_pegawai } = req;
-      if (jabatan_pegawai !== "pegawai") {
+      const { user } = req;
+      if (user.role !== "pegawai") {
         return res
           .status(403)
           .json({ success: false, message: "Unauthorized access" });
@@ -66,7 +91,7 @@ const rekammedisController = {
       let result;
       switch (action) {
         case "create":
-          result = await prisma.rekammedis.create({
+          result = await prisma.rekamMedis.create({
             data: {
               keluhan: data.keluhan,
               diagnosa: data.diagnosa,
@@ -74,17 +99,20 @@ const rekammedisController = {
             },
           });
           break;
+
         case "read":
-          result = await prisma.rekammedis.findMany();
+          result = await prisma.rekamMedis.findMany();
           break;
+
         case "update":
-          result = await prisma.rekammedis.update({
+          result = await prisma.rekamMedis.update({
             where: { id_rekam_medis: data.id_rekam_medis },
             data: { ...data },
           });
           break;
+
         case "delete":
-          result = await prisma.hewan.delete({
+          result = await prisma.rekamMedis.delete({
             where: { id_rekam_medis: data.id_rekam_medis },
           });
           break;
@@ -103,8 +131,8 @@ const rekammedisController = {
   pemilikReadRekamMedis: async (req, res) => {
     try {
       // Pastikan user memiliki peran pemilik
-      const { jabatan_pemilik } = req;
-      if (jabatan_pemilik !== "pemilik") {
+      const { user } = req;
+      if (user.role !== "pemilik") {
         return res
           .status(403)
           .json({ success: false, message: "Unauthorized access" });
@@ -112,9 +140,10 @@ const rekammedisController = {
 
       const { action, data } = req.body;
       let result;
+
       switch (action) {
         case "read":
-          result = await prisma.rekammedis.findMany();
+          result = await prisma.rekamMedis.findMany();
           break;
         default:
           return res
